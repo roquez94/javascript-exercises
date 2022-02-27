61eb02239fe40fae1ec59fb724c413b49c9580ab
Roquez Walker <roquez94@gmail.com>
2022-02-26 21:58:51 -0500

    Completed repeatString test JS file

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+let string = '';
+const number = Math.floor(Math.random() * 10);
 
+const repeatString = function(string, number) {
+    let repeatedString = string;
+    if (number == 0){
+        repeatedString = "";
+    } else if (number < 0){
+        repeatedString = 'ERROR';
+    } else {
+        for (let i = 1; i < number; i++){
+            repeatedString += string;
+        } 
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
