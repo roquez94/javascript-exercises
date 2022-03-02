a605bc8a7d78043c20b28197e16a4cf8f9bf16d4
Roquez Walker <roquez94@gmail.com>
2022-03-01 19:39:46 -0500

    Completed exercise 3 reverse string, working on 4th

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+let String = '';
 
+const reverseString = function() {
+    let word = String.split("");
+    let reverse = word.reverse();
+    let reverseWord = reverse.join("");
+
+    return reverseWord;
 };
 
 // Do not edit below this line
