b2686107756c7fe4bdf36ff0a82d6543cf5361a6
Roquez Walker <roquez94@gmail.com>
2022-03-01 21:58:15 -0500

    Completed ex 4, use from ...restParameters to help with arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...theArgs) {
+    const array = theArgs[0];
+    //creat a new empty array
+    const toCut = [];
+  // use forEach to go through the array
+  array.forEach((item)=> {
+    // push every element into the new toCut array
+    // UNLESS it is included in the function arguments
+    // so we create a new array with every item, except those that should be removed
+        if (!theArgs.includes(item)) {
+            toCut.push(item);
+        }        
+            });
+    return toCut;
 };
 
 // Do not edit below this line
