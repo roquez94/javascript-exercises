3ecff64ad035cf8c79e3c057237a97bdb4241d2a
Roquez Walker <roquez94@gmail.com>
2022-03-02 21:33:38 -0500

    5th exercise done with sum all checking if number is an integer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+    let sum = 0;
+    let low;
+    let high;
 
+    if (first < second){
+        low = first;
+        high = second;
+    } else {
+        low = second;
+        high = first;
+    }
+
+    if (!Number.isInteger(first) || !Number.isInteger(second) 
+        || first < 0 || second < 0){
+        //first == NaN || second == NaN 
+        //|| Array.isArray(first) || Array.isArray(second)
+        sum = "ERROR";
+    } else {
+        for (let i = low; i <= high; i++){
+            sum += +i; 
+        }
+    }
+
+        return sum;
 };
 
 // Do not edit below this line
