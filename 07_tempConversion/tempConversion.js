1864a1a16ee3bf21d4c2599ee9fa2e965c58f5fb
Roquez Walker <roquez94@gmail.com>
2022-03-04 17:35:14 -0500

    Completed ex7, found formulas needed for conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(fah) {
+  let cel = (fah - 32)*(5/9);
+  let roundcel = Math.round(cel*10)/10 ;
 
+  return roundcel;
 };
 
-const ctof = function() {
+const ctof = function(cel) {
+  let fah = (cel*1.8) + 32;
+  let roundfah = Math.round(fah*10)/10 ;
 
+  return roundfah;
 };
 
 // Do not edit below this line
