1dfa3b5b794c3ab2caa0dcfb926ff4f9bcd2a66a
Roquez <roquez94@gmail.com>
2022-04-11 23:07:47 -0400

    Completed calc exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+//use command npm test calculator.spec.js to run testing
+
+const add = function(first, second) {
+	let sum = first + second;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	let diff = first - second;
+  return diff;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sumArray = 0;
+  for (let key in array) {
+    sumArray += array[key];
+  }
+  return sumArray;
 };
 
-const multiply = function() {
-
+function multiply(mult) {
+  let product = 1;
+  for (let number in mult) {
+    product *= mult[number];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(first,second) {
+	let powNum = 1;
+  for (let i = 1; i <= second;  i++) {
+    powNum *= first;
+  }
+  return powNum;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let fact = 1;
+    for (let n = 0; n < number; n++) {
+      fact *= (number - n);
+    }
+    return fact;
 };
 
 // Do not edit below this line
