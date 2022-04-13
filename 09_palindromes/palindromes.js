3f7fbad4ffe4a6c7d546655bc03c56078d87410c
Roquez <roquez94@gmail.com>
2022-04-12 22:42:09 -0400

    Completed 9th practice

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    let normalWord = word.toLowerCase().replace(/[^a-z]/g, "").split("").join("");
+    let reverseWord = word.toLowerCase().replace(/[^a-z]/g, "").split("").reverse().join("");
+
+    return normalWord == reverseWord;
+  
 };
 
 // Do not edit below this line
