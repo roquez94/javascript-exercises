04216896da295fa0dc4aca7eaa12eb80457be0c0
Roquez <roquez94@gmail.com>
2022-04-13 23:01:41 -0400

    Finished fib 9

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(string) {
+    let fib = 0
+    let fib1 = 1; 
 
+    if (string == 0) {
+        return fib;
+    } else if (string < 0) {
+        return "OOPS";
+    } else { 
+    for(n = 1 ; n <= string ; n++){
+        let temp = fib;
+        fib = fib + fib1;
+        fib1 = temp;
+    }
+}
+
+    return fib;
 };
 
 // Do not edit below this line
