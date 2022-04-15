0768f10aa9f64ca8974b8b7361658ab624e2424e
Roquez <roquez94@gmail.com>
2022-04-14 22:01:48 -0400

    completed ex11, use map not filter

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    // let titles = books.map(o=>{
+    //     books["title"];
+    //    });
 
+    return books.map(book => book['title']);
 };
+
+  // map iterates through each object in the array, and applies our arrow function
+  // Array.map() method creates a new array from the results of calling a function for every element
 
 // Do not edit below this line
 module.exports = getTheTitles;
