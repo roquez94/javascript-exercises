c914017560d023219e529a39017b16c772eda732
Roquez <roquez94@gmail.com>
2022-04-16 11:42:16 -0400

    completed ex12, used sort instead of reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let oldPerson = people.sort((a , z) => {
+        //thisYear grabs current year if person is still alive
+        let thisYear = new Date().getFullYear();
+        // let age = (people["yearOfDeath"] || thisYear) - people["yearOfBirth"]; 
+       
+        let lastGuy = (a["yearOfDeath"] || thisYear) - a["yearOfBirth"];
+        let nextGuy = (z["yearOfDeath"] || thisYear) - z["yearOfBirth"];
+
+
+        if (lastGuy > nextGuy){
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+    return people[0];
 };
 
 // Do not edit below this line
